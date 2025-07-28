f727793f45316fae3370c3a3cde0a15dbceca6db
seowiew <seowieww@gmail.com>
2025-07-28 12:34:24 +0600

    month 6 hw7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -91,25 +91,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-#DATABASES = {
-    ##'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': os.getenv('POSTGRES_DB'),
-        #'USER': os.getenv('POSTGRES_USER'),
-        #'PASSWORD': '111',
-        #'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        #'PORT': os.getenv('POSTGRES_PORT', '5432'),
-    #}
-#}
+#Database
+#https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': '111',
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
